718400187b86ba4ec57d615ad3fa584c91e16589
Niklas Langes <niklas.langes@hs-weingarten.de>
2022-11-22 18:00:54 +0100

    before vue 2 tryout again

--- backupmanager/src/components/Login.vue
+++ backupmanager/src/components/Login.vue
@@ -1,22 +1,44 @@
 <template>
-    <form>
-      <h3>Login</h3>
-  
-      <div class="from-group">
-        <label> Email</label>
-        <input type="email" class="form-control" placeholder="Email" />
-      </div>
-  
-      <div>
-        <label> Password</label>
-        <input type="password" class="form-control" placeholder="Password" />
-      </div>
-  
-    </form>
-  </template>
-  
-  <script>
-  export default {
-    name: "Login",
-  };
-  </script>
+  <form>
+    <h3>Login</h3>
+
+    <div class="from-group">
+      <label> Email</label>
+      <input
+        type="email"
+        class="form-control"
+        placeholder="Email"
+        v-model="username"
+      />
+    </div>
+
+    <div>
+      <label> Password</label>
+      <input
+        type="password"
+        class="form-control"
+        placeholder="Password"
+        v-model="password"
+      />
+    </div>
+
+    <button class="btn btn-primary btn-block" @click="handleLogin">Login{{password}}</button>
+
+  </form>
+</template>
+
+<script>
+
+let password = ""
+console.log("1. " + password );
+
+
+export default {
+  name: "Login",
+  methods: {
+    handleLogin() {
+      //console.log(username+ password + "123");
+    },
+  },
+};
+</script>
